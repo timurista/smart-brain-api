1ac9bf7fac9944cf9fef57a3cf18a557bb7cb125
Tim Urista <timothy.urista@gmail.com>
2018-08-30 07:46:47 -0700

    update seed data to simple a user

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
-INSERT into login (hash, email) values ('$2a$04$CMYb6c/xvQQxmfS3oq6ZsusugGeriM13ktGvxM8x/bhMpm8A8RmtO', 'jessie@gmail.com');
+INSERT into users (name, email, entries, joined) values ('a', 'a@gmail.com', 5, '2018-01-01');
+INSERT into login (hash, email) values ('$2a$04$210VXxj0UP30yo994ASXFOJCfoT1Su5GgnU9AA6Bkl1leQBFiRvEC', 'a@gmail.com');
 
 COMMIT TRANSACTION;
