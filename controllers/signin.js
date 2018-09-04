f7edcc5c44c2908554965d8349cd771db57c9525
Tim Urista <timothy.urista@gmail.com>
2018-09-04 06:59:48 -0700

    fix auth token by adding req res

--- controllers/signin.js
+++ controllers/signin.js
@@ -26,7 +26,7 @@ const handleSignin = (db, bcrypt, req, res) => {
     .catch(err => Promise.reject('wrong credentials'))
 }
 
-const getAuthTokenId = () => {
+const getAuthTokenId = (req, res) => {
   const { authorization } = req.headers;
   redisClient.get(authorization, (err, reply) => {
     if (err || !reply) return res.status(400).json('Unauthorized')
