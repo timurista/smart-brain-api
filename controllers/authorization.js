49880878ffb7269681789d3e2be6a8e5b977f0d9
Tim Urista <timothy.urista@gmail.com>
2018-09-04 20:30:51 -0700

    add authorization middleware

---
+++ controllers/authorization.js
@@ -0,0 +1,14 @@
+const redisClient = require('./signin').redisClient;
+
+const requireAuth = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json('Unauthorized');
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) return res.status(401);
+    return next();
+  });
+};
+
+module.exports = {
+  requireAuth,
+};
